221487bd81a26811b3bcee16a2621b51714d943f
Rexarrior <Rexarrior@yandex.ru>
2018-08-18 12:52:02 +0300

    Adding the MakeButtonSample for Sanya_Zhigulkin

---
+++ Sanya_Zhigulin/MakeButtonSample/MakeButtonSample/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MakeButtonSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.makedButtonPanel = new System.Windows.Forms.Panel();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // makedButtonPanel
+            // 
+            this.makedButtonPanel.AutoScroll = true;
+            this.makedButtonPanel.Location = new System.Drawing.Point(12, 12);
+            this.makedButtonPanel.Name = "makedButtonPanel";
+            this.makedButtonPanel.Size = new System.Drawing.Size(247, 206);
+            this.makedButtonPanel.TabIndex = 0;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(334, 38);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "MakeButton";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 240);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.makedButtonPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel makedButtonPanel;
+        private System.Windows.Forms.Button AddButton;
+    }
+}
+
---
+++ Sanya_Zhigulin/MakeButtonSample/MakeButtonSample/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MakeButtonSample
+{
+    public partial class Form1 : Form
+    {
+        private List<Button> buttons;
+        public Form1()
+        {
+            InitializeComponent();
+            buttons = new List<Button>();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            Button button = new Button();
+            button.Location = buttons.Count == 0 ? new Point(13, 13) :
+                new Point(buttons.Last().Location.X, buttons.Last().Location.Y + 30);
+            button.Name = $"button{buttons.Count}";
+            button.Size = new Size(210, 20);
+            
+            button.TabIndex = buttons.Count + 2; // Because tabIndex 1 for AddButton and tab index count from 1. 
+            button.Text = $"Button № {buttons.Count}";
+            button.UseVisualStyleBackColor = true;
+            button.Click += MakedButtonClick;
+            buttons.Add(button);
+            makedButtonPanel.Controls.Add(button);
+        }
+
+
+
+        private void MakedButtonClick(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Button {((Button) sender).Name} clicked.");
+        }
+    }
+}
