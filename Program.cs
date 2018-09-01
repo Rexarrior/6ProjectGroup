30f522ecba6f35927eb9f5d6107b0bd223fa9d44
TodyTodz <ayakubovskaianna@yandex.ru>
2018-09-01 19:21:20 +0300

    sketch

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CyberLife
+{
+	enum Commands : byte
+	{
+		WhatEnergy, // = ? решить с какого индекса начать
+		SeeAround,	
+		Go,
+		Photosynthes,
+		Eat,
+		TurnAround,
+		DoClone,
+		ShareEnergy
+	}
+	
+	
+	public class GenotypeState
+	{
+			
+		/// <summary>
+        /// Геном-код бота
+        /// </summary>	
+		public byte[] Genom = // ? Достать из метаданных бота с проверкой на наличие
+	
+	
+			
+		/// <summary>
+        /// УТК - указатель текущей команды
+        /// </summary>	
+		public byte YTK = // ? Достать из метаданных бота с проверкой на наличие
+			
+		
+		/// <summary>
+        /// Кол-во энергии бота
+        /// </summary>	
+		public int EnergyState=  // ? Достать из метаданных бота
+		
+		
+		/// <summary>
+        /// Конструктор 
+        /// </summary>	
+		public GenotypeState(){
+			
+			Commands cod;
+			
+			//Если геном еще не сгенерирован
+			Genom[]={cod.Photosynthes, cod.WhatEnergy, ,};
+		
+		}
+
+		
+
+		/// <summary>
+        /// Просчет следующего УТК
+        /// </summary>
+		public void NextYTK()
+		{
+			
+		}
+	}
+}
