2253b85aa166008994d4944a59a994b7d49310fe
TodyTodz <42405933+TodyTodz@users.noreply.github.com>
2018-09-01 19:46:09 +0300

    sketch

--- Program.cs
+++ Program.cs
@@ -19,27 +19,27 @@ namespace CyberLife
 	{
 			
 		/// <summary>
-        /// Геном-код бота
-        /// </summary>	
+		/// Геном-код бота
+		/// </summary>	
 		public byte[] Genom = // ? Достать из метаданных бота с проверкой на наличие
 	
 	
 			
 		/// <summary>
-        /// УТК - указатель текущей команды
-        /// </summary>	
+		/// УТК - указатель текущей команды
+		/// </summary>	
 		public byte YTK = // ? Достать из метаданных бота с проверкой на наличие
 			
 		
 		/// <summary>
-        /// Кол-во энергии бота
-        /// </summary>	
+		/// Кол-во энергии бота
+		/// </summary>	
 		public int EnergyState=  // ? Достать из метаданных бота
 		
 		
 		/// <summary>
-        /// Конструктор 
-        /// </summary>	
+		/// Конструктор 
+		/// </summary>	
 		public GenotypeState(){
 			
 			Commands cod;
@@ -52,8 +52,8 @@ namespace CyberLife
 		
 
 		/// <summary>
-        /// Просчет следующего УТК
-        /// </summary>
+		/// Просчет следующего УТК
+		/// </summary>
 		public void NextYTK()
 		{
 			
