d1a7817251e4986048e407ee40d94bd37cff80cd
deced <samofalov.v@tut.by>
2018-09-13 19:40:59 +0300

    Adding Simple2dVisualizer

---
+++ CyberLifeProject/CyberLife/Simple2DWorld/Simple2dVisualizer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CyberLife.Simple2DWorld
+{
+    public class Simple2dVisualizer : IVisualizer
+    {
+        #region fields
+
+        private Bitmap map;
+
+        #endregion
+
+
+        #region properties
+
+        #endregion
+
+
+        #region methods
+
+        /// <summary>
+        /// Вызывает обновление цветов форм жизни
+        /// </summary>
+        /// <param name="metadata"></param>
+        public void Update(WorldMetadata metadata)
+        {
+            if (map == null)
+                map = new Bitmap(metadata.EnvironmentMetadata.Size.Width, metadata.EnvironmentMetadata.Size.Height);
+            foreach (var pair in metadata)
+            {
+                byte[] bytes = pair.Value["ColorState"]["Color"].Split(' ').Select(x => byte.Parse(x)).ToArray();
+                byte R = bytes[0];
+                byte G = bytes[1];
+                byte B = bytes[2];
+                Color color = Color.FromArgb(R, G, B);
+                map.SetPixel(pair.Value.Place.Points[0].X, pair.Value.Place.Points[0].Y, color);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Получает текущую карту
+        /// </summary>
+        /// <returns></returns>
+        public Bitmap GetMap()
+        {
+            return map;
+        }
+
+        #endregion
+
+
+        #region constructors
+
+        #endregion
+    }
+}
